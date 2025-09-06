4f375ffb5ffacc1da6ab4bb29c55680c3de3de6e
Crispus-256 <nuweamanyacrispusinfinityeneri@gmail.com>
2025-09-06 13:05:01 -0600

    completed exer1 and exer2

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
-    }
-}
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+        int grade = int.Parse(gradePercentage);
+        string letter;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (letter != "F")
+        {
+            int lastDigit = grade % 10;
+
+            if (lastDigit >= 7 && grade != 100)
+            {
+                letter += "+";
+            }
+            else if (lastDigit < 3 && grade != 100)
+            {
+                letter += "-";
+            }
+        
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the course.");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately, you did not pass. Better luck next time!");
+        }
+        
+    }    }   
